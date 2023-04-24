14352cd404bbfd7457fa6c71c49b5e86f8fb861d
Pablo2095 <preciouspablos@gmail.com>
2023-04-24 00:16:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * main - Print all the alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int p;
+
+	for (p = 0; p < 0; p++)
+	{
+		putchar(alp[p]);
+	}
+	putchar('\n');
+	return (0);
+}
