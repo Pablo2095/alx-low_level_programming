f39e970dabdd28cfc79927a8af951341f5b27aa7
Pablo2095 <preciouspablos@gmail.com>
2023-04-24 17:01:20 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print alphabets except q and e
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+	int p =97;
+
+	while (p <= 122)
+	{
+		if (p == 101 || p == 113)
+		{
+			p++;
+			continue;
+		}
+		putchar(p);
+		p++;
+	}
+	putchar('\n');
+	return (0);
+}
