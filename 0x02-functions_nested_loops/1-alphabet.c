451febb35bb02481dcff78bd634b1d12807ba9f6
Pablo2095 <preciouspablos@gmail.com>
2023-04-26 08:34:40 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point.
- * print the alphabet, in lowercase.
+ * main - Entry point
+ * print the lowercase alphabets.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int p = 97;
 	int s = 122;
-	
+
 	for (p = 97; p <= 122; p++)
 	{
 		putchar(p);
